537a1496d51ad9c047838f2c65a372a5f34761e0
DESKTOP-0UO9BQF\AnuradhA <h.supun.anuradha@gmail.com>
2024-08-29 18:06:48 +0530

    clean project files

---
+++ Assets/Scripts/UI/HealthUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthUI : MonoBehaviour
+{
+    public Image heart;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    // public GameObject heart;
+    private List<Image> hearts = new List<Image>();
+
+    // PlayerHealth playerHealth;
+
+    public void SetMaxHearts(int maxHearts)
+    {
+        foreach(Image heart in hearts)
+        {
+            Destroy(heart.gameObject);
+        }
+
+        hearts.Clear();
+
+        for(int i=0; i<maxHearts; i++)
+        {
+            Image newHeart = Instantiate(heart, transform);
+            newHeart.sprite = fullHeart;
+            hearts.Add(newHeart);
+        }
+    }
+
+
+    public void UpdateHearts(int currentHealth)
+    {
+        for(int i=0; i<hearts.Count; i++)
+        {
+            if(i < currentHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else{
+                hearts[i].sprite = emptyHeart;
+            }
+        }
+
+    }
+
+}
